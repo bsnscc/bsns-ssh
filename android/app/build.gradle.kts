84076ca8c43a2156c7e8c18bf00339082d301c6a
Graham Saathoff <graham@bsns.cc>
2026-06-15 22:24:25 -0700

    android: bump to 0.2.3 (versionCode 5)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cc.bsns.ssh"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.2.2"
+        versionCode = 5
+        versionName = "0.2.3"
         ndk { abiFilters += "arm64-v8a" }
     }
 
