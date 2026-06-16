4e47d65a01a7d3a8b4bfb4b6d244cb4c7eeeb184
Graham Saathoff <graham@bsns.cc>
2026-06-15 21:47:29 -0700

    android: bump to 0.2.2 (versionCode 4)

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "cc.bsns.ssh"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.2.1"
+        versionCode = 4
+        versionName = "0.2.2"
         ndk { abiFilters += "arm64-v8a" }
     }
 
