f491548cd078dced9173248174f0a3d120f7facb
Graham Saathoff <graham@bsns.cc>
2026-06-18 13:24:24 -0700

    Android versionCode 22

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "cc.bsns.ssh"
         minSdk = 26
         targetSdk = 35
-        versionCode = 21
+        versionCode = 22
         versionName = "1.0"
         ndk { abiFilters += "arm64-v8a" }
     }
